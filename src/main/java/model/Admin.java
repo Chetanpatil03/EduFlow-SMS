99465f70acc5880046d033f59628310354ef9ff3
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-25 00:27:26 +0530

    created hibernate.cfg.xml for adding hibernate into project and created Admin class for work as admin

--- src/main/java/model/Admin.java
+++ src/main/java/model/Admin.java
@@ -1,5 +1,64 @@
 package model;
 
-public class Admin {
+import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "admin")
+public class Admin {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long admin_id;
+	private String name;
+	private String email;
+	private String pass;
+	
+	@CreationTimestamp
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime created_at;
+	
+	public Admin() {}
+
+	public long getAdmin_id() {
+		return admin_id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getCreated_at() {
+		return created_at.toString();
+	}
+	
+	
 }
